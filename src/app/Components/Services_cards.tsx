e6b6e8a269fee4b254c12d092b69827c351201cd
ManideepRed <162531721+ManideepRed@users.noreply.github.com>
2025-02-01 15:16:47 +0530

    Update Services_cards.tsx

--- src/app/Components/Services_cards.tsx
+++ src/app/Components/Services_cards.tsx
@@ -41,6 +41,48 @@ const ServicesCards: React.FC = () => {
       text: "We take pride in transforming ideas into visually captivating and impactful designs. Our portfolio reflects the essence of innovation, precision, and dedication to delivering exceptional creative solutions",
       subhead: "What We Offer",
       arr: ["Customized Layouts", "Interactive Digital Portfolios", "Print-Ready Designs"]
+    },
+    {
+      img: "./s3.png",
+      head: "Business Card Design",
+      text: "We craft business cards that make a powerful first impression. Whether you're networking, pitching, or meeting clients, our designs ensure your card reflects professionalism and leaves a lasting impact.",
+      subhead: "Our Offerings",
+      arr: ["Custom Designs","Premium Quality","Double-Sided Cards","Innovative Formats"]
+    },
+    {
+      img: "./s4.png",
+      head: "Social Media Management",
+      text: "We craft tailored strategies to boost engagement, build brand visibility, and connect with your audience",
+      subhead: "Our Services",
+      arr: ["Custom Strategies","Social Media Posts","Content Creation","Social Media Ads","Performance Analytics"]
+    },
+    {
+      img: "./s5.png",
+      head: "Web Design",
+      text: "We create custom, responsive websites that deliver exceptional user experiences and align with your business goals",
+      subhead: "Our Services",
+      arr: ["Custom Website Development","Responsive Design","E-commerce Solutions","Website Maintenance"]
+    },
+    {
+      img: "./s6.png",
+      head: "Logo Design",
+      text: "We understand that your logo is more than just a symbol—it’s the heart of your brand. It represents your values, vision, and uniqueness. Our logo designs are thoughtfully crafted to make a lasting impression and resonate with your audience",
+      subhead: "What We Offer",
+      arr: ["Custom Logo Design","Versatile Styles","Timeless Appeal"]
+    },
+    {
+      img: "./s7.png",
+      head: "Magazine Design",
+      text: "We design visually stunning magazines that engage readers and elevate your content. From layout to typography, we bring creativity and professionalism to every page",
+      subhead: "What We Offer",
+      arr: ["Magazine Layout Design","Thematic Cover Designs","Content-Driven Visuals"]
+    },
+    {
+      img: "./s8.png",
+      head: "Video Editing",
+      text: "We craft captivating videos that leave a lasting impression. Whether it’s event highlights, promotional content, or social media reels, our video edits are tailored to tell your story with impact",
+      subhead: "What We Offer",
+      arr: ["Cinematic Video Edits","Social Media Reels","Event Highlights"]
     }
   ];
 
