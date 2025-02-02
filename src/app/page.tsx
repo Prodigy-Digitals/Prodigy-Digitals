25b0018db8a3b633fa3c9fd8182d7a74642afe03
Manideep <alukamanideepreddy@gmail.com>
2025-02-02 14:48:27 +0530

    Fixed typing animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import dynamic from 'next/dynamic';
 import Portfolio from "./Components/Portfolio";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
+import Typewriter from 'typewriter-effect';
 
 const poppins = Poppins({
   subsets: ['latin'], // Use subsets for optimization
@@ -77,8 +78,16 @@ export default function Home() {
             <a className="text-[rgba(249,227,98,1)]">EMPOWERING</a> YOUR
           </div>
           <div className="w-full text-center flex justify-center md:justify-normal">
-           <div className="font-extrabold text-[2.5vh] md:text-[3.5vh] lg:text-[5vh] flex-shrink-0 border-r-4 pr-1 max-w-max animate-typing overflow-hidden">
-             {stringArray[currentText]}
+           <div className="font-extrabold text-[2.5vh] md:text-[3.5vh] lg:text-[5vh] flex-shrink-0 border-r-4 animate-typing pr-1 max-w-max overflow-hidden">
+                <Typewriter
+                  options={{
+                    strings: ['JOURNEY', 'VISION'],
+                    autoStart: true,
+                    loop: true,
+                    cursor: '',
+                    deleteSpeed:100
+                  }}
+                />
            </div>
           </div>
 
